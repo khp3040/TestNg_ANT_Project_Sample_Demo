17975cdd2ad12a1bc11a06e031862e1ba03aed00
KSHITIJ PAWAR <31236146+khp3040@users.noreply.github.com>
2021-05-01 00:27:27 +0530

    TestNG-ANT-Project

---
+++ src/test/SampleBuildTest.java
@@ -0,0 +1,11 @@
+package test;
+ import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+public class SampleBuildTest {
+    
+    @Test
+    public void testMethodOne() {
+        AssertJUnit.assertTrue(true);
+        
+    }
+}
